4fab9eba6afe6f6ef306bd40aafd89455a9e00f5
Hyu <78741149+hyuyu104@users.noreply.github.com>
2021-12-25 17:48:42 -0600

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int x = 0;
+		while (x < 10) {
+			System.out.print(x + " ");
+			x = x + 1;
+		}
+		System.out.println();
+		System.out.println(5 + 10 + "h");
+		System.out.println("h" + 5 + 10);
+	}
+}
