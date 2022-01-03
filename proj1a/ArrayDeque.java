3e46e4ec7ee30eb7a95122f302a49bb88e4f7185
Hyu <78741149+hyuyu104@users.noreply.github.com>
2022-01-03 12:39:11 -0600

    gradescope first version, deleted deep copy

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,129 +1,144 @@
-public class ArrayDeque<T>{
-	private T[] items;
-	private int size;
-	private int fsize;
-	private int bsize;
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
+    private int fsize;
+    private int bsize;
 
-	/* The constructor of ArrayDeque, will create an empty array of size 8.*/
-	public ArrayDeque() {
-		items = (T[]) new Object[8];
-		fsize = 0;
-		bsize = 0;
-	}
+    /* The constructor of ArrayDeque, will create an empty array of size 8.*/
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        fsize = 0;
+        bsize = 0;
+    }
 
-	/**
-	 * The constructor of ArrayDeque, will do a deep copy of other.
-	 * @other: the ArrayDeque to copy from*/
-	public ArrayDeque(ArrayDeque other) {
-		items = (T[]) new Object[8];
-		fsize = 0;
-		bsize = 0;
+    /**
+     * The constructor of ArrayDeque, will do a deep copy of other.
+     * @other: the ArrayDeque to copy from*/
+    // public ArrayDeque(ArrayDeque other) {
+    //     items = (T[]) new Object[8];
+    //     fsize = 0;
+    //     bsize = 0;
 
-		for (int i = 0; i < other.size(); i++) {
-			addLast((T) other.get(i));
-		}
-	}
+    //     for (int i = 0; i < other.size(); i++) {
+    //         addLast((T) other.get(i));
+    //     }
+    // }
 
-	/**
-	 * resize items according to the length specified
-	 * @capacity: the expected length after resizing*/
-	private void resize(int capacity) {
-		T[] a = (T[]) new Object[capacity];
-		System.arraycopy(items, 0, a, 0, bsize);
-		System.arraycopy(items, items.length-fsize, a, a.length-fsize, fsize);
-		items = a;
-	}
+    /**
+     * resize items according to the length specified
+     * @capacity: the expected length after resizing*/
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        System.arraycopy(items, 0, a, 0, bsize);
+        System.arraycopy(items, items.length - fsize, a, a.length - fsize, fsize);
+        items = a;
+    }
 
-	/**
-	 * addFirst will add an item of type T to the first postion of the list.
-	 * @item: item to be added of type T*/
-	public void addFirst(T item) {
-		size = bsize + fsize;
-		if (size >= items.length) {
-			resize(size * 2);
-		}
-		items[items.length - fsize - 1] = item;
-		fsize = fsize + 1;
-	}
+    /**
+     * addFirst will add an item of type T to the first postion of the list.
+     * @item: item to be added of type T*/
+    public void addFirst(T item) {
+        size = bsize + fsize;
+        if (size >= items.length) {
+            resize(size * 2);
+        }
+        items[items.length - fsize - 1] = item;
+        fsize = fsize + 1;
+    }
 
-	/**
-	 * addLast will add an item of type T to the last postion of the list.
-	 * @item: item to be added of type T*/
-	public void addLast(T item) {
-		size = bsize + fsize;
-		if (size >= items.length) {
-			resize(size * 2);
-		}
-		items[bsize] = item;
-		bsize = bsize + 1;
-	}
+    /**
+     * addLast will add an item of type T to the last postion of the list.
+     * @item: item to be added of type T*/
+    public void addLast(T item) {
+        size = bsize + fsize;
+        if (size >= items.length) {
+            resize(size * 2);
+        }
+        items[bsize] = item;
+        bsize = bsize + 1;
+    }
 
-	/**
-	 * saveResize will resize the list when usage ratio is less than 0.25
-	 * and the length of the list is greater than or equal to 16 in order
-	 * to save memory*/
-	private void saveResize() {
-		size = bsize + fsize;
-		if (size/items.length < 0.25 & items.length >= 16) {
-			resize(size + 1);
-		}
-	}
+    /**
+     * saveResize will resize the list when usage ratio is less than 0.25
+     * and the length of the list is greater than or equal to 16 in order
+     * to save memory*/
+    private void saveResize() {
+        size = bsize + fsize;
+        if (size/items.length < 0.25 & items.length >= 16) {
+            resize(size + 1);
+        }
+    }
 
-	/**
-	 * removeFirst will remove the first item from the list and return it.
-	 * @return: item removed of type T*/
-	public T removeFirst() {
-		T returnItem = items[items.length - fsize];
-		items[items.length - fsize] = null;
-		fsize = fsize - 1;
-		saveResize();
-		return returnItem;
-	}
+    /**
+     * Transform the index into index corresponding to the underlying array
+     * @index: integer, input index
+     * @return: index corresponding to the underlying array*/
+    private int indexer(int index) {
+        if (index < fsize) {
+            fsize = fsize - 1;
+            return items.length - fsize + index;
+        }
+        bsize = bsize - 1;
+        return index - fsize;
+    }
 
-	/**
-	 * removeLast will remove the last item from the list and return it.
-	 * @return: item removed of type T*/
-	public T removeLast() {
-		T returnItem = items[bsize - 1];
-		items[bsize - 1] = null;
-		bsize = bsize - 1;
-		saveResize();
-		return returnItem;
-	}
+    /**
+     * removeFirst will remove the first item from the list and return it.
+     * @return: item removed of type T*/
+    public T removeFirst() {
+        T returnItem = items[indexer(0)];
+        items[indexer(0)] = null;
+        saveResize();
+        return returnItem;
+    }
 
-	/**
-	 * isEmpty will determine whether the list is empty.
-	 * @return: boolean value based on whether the list is empty.*/
-	public boolean isEmpty() {
-		return (fsize + bsize) == 0;
-	}
+    /**
+     * removeLast will remove the last item from the list and return it.
+     * @return: item removed of type T*/
+    public T removeLast() {
+        T returnItem = items[indexer(bsize + fsize - 1)];
+        items[indexer(bsize + fsize - 1)] = null;
+        saveResize();
+        return returnItem;
+    }
 
-	/**
-	 * size will return the current size of the list.
-	 * @return: an integer of the size of the list*/
-	public int size() {
-		return fsize + bsize;
-	}
+    /**
+     * isEmpty will determine whether the list is empty.
+     * @return: boolean value based on whether the list is empty.*/
+    public boolean isEmpty() {
+        return (fsize + bsize) <= 0;
+    }
 
-	/**
-	 * get method will return the value of ith item.
-	 * @index: the index of the item, integer
-	 * @return: the value of the ith item with a type of T*/
-	public T get(int index) {
-		if (index < fsize) {
-			return items[items.length - fsize + index];
-		}
-		return items[index - fsize];
-	}
+    /**
+     * size will return the current size of the list.
+     * @return: an integer of the size of the list*/
+    public int size() {
+    	size = bsize + fsize;
+    	if (size <= 0) {
+    		return 0;
+    	}
+        return size;
+    }
 
-	/* printDeque will print each item in the list, separating by space*/
-	public void printDeque() {
-		for (int i = items.length - fsize; i < items.length; i++) {
-			System.out.print(items[i] + " ");
-		}
-		for (int i = 0; i < bsize; i++) {
-			System.out.print(items[i] + " ");
-		}
-		System.out.println();
-	}
+    /**
+     * get method will return the value of ith item.
+     * @index: the index of the item, integer
+     * @return: the value of the ith item with a type of T*/
+    public T get(int index) {
+        if (index < fsize) {
+            return items[items.length - fsize + index];
+        }
+        return items[index - fsize];
+    }
+
+    /* printDeque will print each item in the list, separating by space*/
+    public void printDeque() {
+        for (int i = items.length - fsize; i < items.length; i++) {
+            System.out.print(items[i] + " ");
+        }
+        for (int i = 0; i < bsize; i++) {
+            System.out.print(items[i] + " ");
+        }
+        System.out.println();
+    }
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,141 +1,146 @@
 public class LinkedListDeque<T>{
 
-	public class Node{
+    private class Node{
 
-		public T item;
-		public Node prev;
-		public Node next;
+        public T item;
+        public Node prev;
+        public Node next;
 
-		/**
-		 * This is the constructor of Node.
-		 * @i: item in each Node with type of T
-		 * @p: reference to the previous Node
-		 * @n: reference to the next Node
-		 */
-		public Node(Node p, T i, Node n) {
-			prev = p;
-			item = i;
-			next = n;
-		}
-	}
+        /**
+         * This is the constructor of Node.
+         * @i: item in each Node with type of T
+         * @p: reference to the previous Node
+         * @n: reference to the next Node
+         */
+        public Node(Node p, T i, Node n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
 
-	private int size;
-	private Node sentinel;
+    private int size;
+    private Node sentinel;
 
-	/* The constructor of LinkedListDeque, will create an empty list.*/
-	public LinkedListDeque() {
-		size = 0;
-		sentinel = new Node(null, null, null);
-		sentinel.prev = sentinel;
-		sentinel.next = sentinel;
-	}
+    /* The constructor of LinkedListDeque, will create an empty list.*/
+    public LinkedListDeque() {
+        size = 0;
+        sentinel = new Node(null, null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+    }
 
-	/**
-	 * The constructor of LinkedListDeque, will do a deep copy of other.
-	 * @other: the LinkedListDeque to copy from*/
-	public LinkedListDeque(LinkedListDeque other) {
-		size = 0;
-		sentinel = new Node(null, null, null);
-		sentinel.prev = sentinel;
-		sentinel.next = sentinel;
+    /**
+     * The constructor of LinkedListDeque, will do a deep copy of other.
+     * @other: the LinkedListDeque to copy from*/
+    // public LinkedListDeque(LinkedListDeque other) {
+    //     size = 0;
+    //     sentinel = new Node(null, null, null);
+    //     sentinel.prev = sentinel;
+    //     sentinel.next = sentinel;
 
-		for (int i = 0; i < other.size(); i++) {
-			addLast((T) other.get(i));
-		}
-	}
+    //     for (int i = 0; i < other.size(); i++) {
+    //         addLast((T) other.get(i));
+    //     }
+    // }
 
-	/**
-	 * addFirst will add an item of type T to the first postion of the list.
-	 * @item: item to be added of type T*/
-	public void addFirst(T item) {
-		sentinel.next = new Node(sentinel, item, sentinel.next);
-		sentinel.next.next.prev = sentinel.next;
-		size = size + 1;
-	}
+    /**
+     * addFirst will add an item of type T to the first postion of the list.
+     * @item: item to be added of type T*/
+    public void addFirst(T item) {
+        sentinel.next = new Node(sentinel, item, sentinel.next);
+        sentinel.next.next.prev = sentinel.next;
+        size = size + 1;
+    }
 
-	/**
-	 * addLast will add an item of type T to the last postion of the list.
-	 * @item: item to be added of type T*/
-	public void addLast(T item) {
-		sentinel.prev = new Node(sentinel.prev, item, sentinel);
-		sentinel.prev.prev.next = sentinel.prev;
-		size = size + 1;
-	}
+    /**
+     * addLast will add an item of type T to the last postion of the list.
+     * @item: item to be added of type T*/
+    public void addLast(T item) {
+        sentinel.prev = new Node(sentinel.prev, item, sentinel);
+        sentinel.prev.prev.next = sentinel.prev;
+        size = size + 1;
+    }
 
-	/**
-	 * removeFirst will remove the first item from the list and return it.
-	 * @return: item removed of type T*/
-	public T removeFirst() {
-		T returnItem = sentinel.next.item;
-		sentinel.next = sentinel.next.next;
-		sentinel.next.prev = sentinel;
-		size = size - 1;
-		return returnItem;
-	}
+    /**
+     * removeFirst will remove the first item from the list and return it.
+     * @return: item removed of type T*/
+    public T removeFirst() {
+        T returnItem = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size = size - 1;
+        return returnItem;
+    }
 
-	/**
-	 * removeLast will remove the last item from the list and return it.
-	 * @return: item removed of type T*/
-	public T removeLast() {
-		T returnItem = sentinel.prev.item;
-		sentinel.prev = sentinel.prev.prev;
-		sentinel.prev.next = sentinel;
-		size = size - 1;
-		return returnItem;
-	}
+    /**
+     * removeLast will remove the last item from the list and return it.
+     * @return: item removed of type T*/
+    public T removeLast() {
+        T returnItem = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size = size - 1;
+        return returnItem;
+    }
 
-	/**
-	 * isEmpty will determine whether the list is empty.
-	 * @return: boolean value based on whether the list is empty.*/
-	public boolean isEmpty() {
-		return size==0;
-	}
+    /**
+     * isEmpty will determine whether the list is empty.
+     * @return: boolean value based on whether the list is empty.*/
+    public boolean isEmpty() {
+        return size <= 0;
+    }
 
-	/**
-	 * size will return the current size of the list.
-	 * @return: an integer of the size of the list*/
-	public int size() {
-		return size;
-	}
+    /**
+     * size will return the current size of the list.
+     * @return: an integer of the size of the list*/
+    public int size() {
+    	if (size <= 0) {
+    		return 0;
+    	}
+        return size;
+    }
 
-	/**
-	 * get method will return the value of ith item using iteration.
-	 * @index: the index of the item, integer
-	 * @return: the value of the ith item with a type of T*/
-	public T get(int index) {
-		Node a = sentinel;
-		while (index >= 0) {
-			a = a.next;
-			index = index - 1;
-		}
-		return a.item;
-	}
+    /**
+     * get method will return the value of ith item using iteration.
+     * @index: the index of the item, integer
+     * @return: the value of the ith item with a type of T*/
+    public T get(int index) {
+        Node a = sentinel;
+        while (index >= 0) {
+            a = a.next;
+            index = index - 1;
+        }
+        return a.item;
+    }
 
-	/**
-	 * This is a helper function of the getRecursive method.
-	 * @index: the index of the item, integer
-	 * @Node: Node where getRecursive will find item
-	 * @return: the value of the ith item with a type of T*/
-	private T getRecursive(int index, Node n) {
-		if (index < 0) return n.item;
-		return getRecursive(index - 1, n.next);
-	}
+    /**
+     * This is a helper function of the getRecursive method.
+     * @index: the index of the item, integer
+     * @Node: Node where getRecursive will find item
+     * @return: the value of the ith item with a type of T*/
+    private T getRecursive(int index, Node n) {
+        if (index < 0) {
+        	return n.item;
+        }
+        return getRecursive(index - 1, n.next);
+    }
 
-	/**
-	 * getRecursive method will return the value of ith item using recursion.
-	 * @index: the index of the item, integer
-	 * @return: the value of the ith item with a type of T*/
-	public T getRecursive(int index) {
-		return getRecursive(index, sentinel);
-	}
+    /**
+     * getRecursive method will return the value of ith item using recursion.
+     * @index: the index of the item, integer
+     * @return: the value of the ith item with a type of T*/
+    public T getRecursive(int index) {
+        return getRecursive(index, sentinel);
+    }
 
-	/* printDeque will print each item in the list, separating by space*/
-	public void printDeque() {
-		Node a = sentinel;
-		for (int i = 0; i < size; i ++) {
-			System.out.print(a.next.item + " ");
-			a = a.next;
-		}
-		System.out.println();
-	}
+    /* printDeque will print each item in the list, separating by space*/
+    public void printDeque() {
+        Node a = sentinel;
+        for (int i = 0; i < size; i++) {
+            System.out.print(a.next.item + " ");
+            a = a.next;
+        }
+        System.out.println();
+    }
 }
