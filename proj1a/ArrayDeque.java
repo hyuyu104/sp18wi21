bbc4a3ec78912d21ed734291651e841fa1d525b3
Hyu <78741149+hyuyu104@users.noreply.github.com>
2022-01-19 14:52:55 -0600

    ArrayDeque, one-time arraycopy, fixed resize, saveResize, negative size

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,155 +1,131 @@
-/* Solution by https://github.com/seriouszyx/CS61BCN/blob/master/proj1a/ArrayDeque.java*/
 public class ArrayDeque<T> {
     private T[] items;
     private int size;
-    private int nextFirst;
-    private int nextLast;
-    private final int INITIAL_CAPACITY = 8;
+    private int findex;
 
+    /* The constructor of ArrayDeque, will create an empty array of size 8.*/
     public ArrayDeque() {
-        items = (T[]) new Object[INITIAL_CAPACITY];
+        items = (T[]) new Object[8];
         size = 0;
-        nextFirst = 0;
-        nextLast = 1;
+        findex = 1;
     }
 
+    /**
+     * The constructor of ArrayDeque, will do a deep copy of other.
+     * @other: the ArrayDeque to copy from*/
+    // public ArrayDeque(ArrayDeque other) {
+    //     items = (T[]) new Object[8];
+    //     size = 0;
+    //     findex = 1;
+
+    //     for (int i = 0; i < other.size(); i++) {
+    //         addLast((T) other.get(i));
+    //     }
+    // }
+
+    /**
+     * resize items according to the length specified
+     * @capacity: the expected length after resizing*/
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        int fidxNew = (capacity - size) / 2;
+        System.arraycopy(items, findex, a, fidxNew, size);
+        items = a;
+        findex = fidxNew;
+    }
+
+    /**
+     * addFirst will add an item of type T to the first postion of the list.
+     * @item: item to be added of type T*/
+    public void addFirst(T item) {
+        if (findex == 0) {
+            resize((size + 1) * 2);
+        }
+        items[findex - 1] = item;
+        findex = findex - 1;
+        size = size + 1;
+    }
+
+    /**
+     * addLast will add an item of type T to the last postion of the list.
+     * @item: item to be added of type T*/
+    public void addLast(T item) {
+        if ((findex + size) >= items.length) {
+            resize((size + 1) * 2);
+        }
+        items[findex + size] = item;
+        size = size + 1;
+    }
+
+    /**
+     * saveResize will resize the list when usage ratio is less than 0.25
+     * and the length of the list is greater than or equal to 16 in order
+     * to save memory*/
+    private void saveResize() {
+        if (size / items.length < 0.25 & items.length >= 16) {
+            resize((size + 1) * 2);
+        }
+    }
+
+    /**
+     * removeFirst will remove the first item from the list and return it.
+     * @return: item removed of type T*/
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        T returnItem = items[findex];
+        items[findex] = null;
+        findex = findex + 1;
+        size = size - 1;
+        saveResize();
+        return returnItem;
+    }
+
+    /**
+     * removeLast will remove the last item from the list and return it.
+     * @return: item removed of type T*/
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        T returnItem = items[findex + size - 1];
+        items[findex + size - 1] = null;
+        size = size - 1;
+        saveResize();
+        return returnItem;
+    }
+
+    /**
+     * isEmpty will determine whether the list is empty.
+     * @return: boolean value based on whether the list is empty.*/
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * size will return the current size of the list.
+     * @return: an integer of the size of the list*/
     public int size() {
+        if (size <= 0) {
+            size = 0;
+        }
         return size;
     }
 
-    public boolean isEmpty() {
-        return (size == 0 ? true : false);
-    }
-
-    public int minusOne(int index) {
-        return Math.floorMod(index-1, items.length);
-    }
-
-
-    public int plusOne(int index) {
-        return Math.floorMod(index+1, items.length);
-    }
-
-    public int plusOne(int index, int length) {
-        return Math.floorMod(index+1, length);
-    }
-
     /**
-     *  invariants:
-     *      设计resize()方法，将在增加ArrayDeaue实例内存的方法中调用
-     *      内部判断内存满则调用expand()增加内存
-     *        如果内存过小则调用reduce()减小内存
-     **/
-
-    private void resize() {
-        if (size == items.length) {
-            expand();
-        }
-        if (size < items.length / 4 && items.length > 8) {
-            reduce();
-        }
+     * get method will return the value of ith item.
+     * @index: the index of the item, integer
+     * @return: the value of the ith item with a type of T*/
+    public T get(int index) {
+        return items[findex + index];
     }
 
-    private void expand() {
-        resizeHelper(items.length * 2);
-    }
-
-    private void reduce() {
-        resizeHelper(items.length / 2);
-    }
-
-    private void resizeHelper(int capacity) {
-        T[] temp = items;
-        int begin = plusOne(nextFirst);
-        int end = minusOne(nextLast);
-        items = (T[]) new Object[capacity];
-        nextFirst = 0;
-        nextLast = 1;
-        for (int i=begin; i != end; i = plusOne(i, temp.length)) {
-            items[nextLast] = temp[i];
-            nextLast = plusOne(nextLast);
-        }
-        items[nextLast] = temp[end];
-        nextLast = plusOne(nextLast);
-    }
-
-    /**
-     *  invariants:
-     *      通过minusOne()方法确定nextFirst，(nextFirst-1)%items.length
-     *      即nextFirst的下一个位置
-     *      eg. (0 - 1) % 8 = 7
-     *
-     * */
-    public void addFirst(T item) {
-        // resize();
-        items[nextFirst] = item;
-        nextFirst = minusOne(nextFirst);
-        size++;
-    }
-
-    public T getFirst() {
-        return items[plusOne(nextFirst)];
-    }
-
-    public T removeFirst() {
-        resize();
-        T res = getFirst();
-        nextFirst = plusOne(nextFirst);
-        items[nextFirst] = null;
-        size--;
-        return res;
-    }
-
-    public void addLast(T item) {
-        resize();
-        items[nextLast] = item;
-        nextLast = plusOne(nextLast);
-        size++;
-    }
-
-    public T getLast() {
-        return items[minusOne(nextLast)];
-    }
-
-    public T removeLast() {
-        resize();
-        T res = getLast();
-        nextLast = minusOne(nextLast);
-        items[nextLast] = null;
-        size--;
-        return res;
-    }
-
+    /* printDeque will print each item in the list, separating by space*/
     public void printDeque() {
-        for (int index = plusOne(nextFirst); index != nextLast; index = plusOne(index)) {
-            System.out.print(items[index]);
-            System.out.print(" ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[findex + i] + " ");
         }
         System.out.println();
     }
-
-    public T get(int index) {
-        if (index < 0 || index >= size) {
-            return null;
-        }
-        index = Math.floorMod(plusOne(nextFirst) + index, items.length);
-        return items[index];
-    }
-
-
-
-    /*
-    public static void main(String[] args) {
-        ArrayDeque<Integer> aq = new ArrayDeque<Integer>();
-        for (int i = 0; i < 100; i++) {
-            aq.addLast(i);
-        }
-        aq.printDeque();
-        for (int i = 0; i < 98; i++) {
-            aq.removeFirst();
-        }
-        aq.printDeque();
-        System.out.println(aq.get(0));
-    }
-    */
 }
